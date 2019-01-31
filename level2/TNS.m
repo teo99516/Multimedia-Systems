fd0814b51ef5c07eafc456f3a4be180007fdabbe
teo99516 <40856983+teo99516@users.noreply.github.com>
2019-01-31 17:34:47 +0200

    Add files via upload

--- level2/TNS.m
+++ level2/TNS.m
@@ -1,4 +1,4 @@
-function [ frameFout] = TNS(frameFin, frameType)
+function [ frameFout, steady, TNScoeffs ] = TNS(frameFin, frameType)
 
 %frameFin,frameFout: MDCT coefficiences: 128X8 for ESH, else 1024X1 
 
@@ -7,10 +7,12 @@ function [ frameFout] = TNS(frameFin, frameType)
     table=load('TableB219.mat');
     long_fft=table.B219a;
     short_fft=table.B219b;
+    steady=1;
     %Calculate normalization coefficiences according to Table B219a
     if frameType=="ESH"
         %Calculate 42 values of band energies for every subframe
         normalization_coef=zeros(128,8);
+        frameFout=zeros(128,8);
         for j=1:8
             for i=1:42
                 %Calculate a new array for the squared values of frameFin, 
@@ -18,6 +20,7 @@ function [ frameFout] = TNS(frameFin, frameType)
                 %by column wlow(longfft(i,2)) and the following one wlow(long_fff(i+1,2) )
                 %In each index add one for matlab compatibility
                 band_energy = sum((frameFin(short_fft(i,2)+1:short_fft(i,3)+1, j) ).^2);
+           
                 
                 %Callculate the normalization coefficiencies of the selected values
                 %by taking the root square of band's energy
@@ -25,18 +28,43 @@ function [ frameFout] = TNS(frameFin, frameType)
             end
              %Make values less steep  
             for i=127:-1:1
-                normalization_coef(i)=( normalization_coef(i, j) + normalization_coef(i+1, j) )/2;
+                normalization_coef(i,j)=( normalization_coef(i, j) + normalization_coef(i+1, j) )/2;
             end
+            normalized_mdct=zeros(128,1);
+            normalized_mdct(1)=frameFin(1,j)/normalization_coef(1,j);
             for i=2:128
-                normalization_coef(i)=( normalization_coef(i, j) + normalization_coef(i-1, j) )/2;
+                normalization_coef(i,j)=( normalization_coef(i, j) + normalization_coef(i-1, j) )/2;
+                normalized_mdct(i)=frameFin(i,j)/normalization_coef(i,j);
             end 
+
+            %Calculate the 4 optimized coeffieciencies a of the FIR prediction filter
+            optimized_a_coef = lpc(normalized_mdct,4);
+            
+            optimized_a_coef=transpose(optimized_a_coef);
+
+            %Quantize the FIR coefficiencies
+            for i=2:5
+                optimized_a_coef(i)=quanti( optimized_a_coef(i) );
+            end
+            
+            %Apply FIR filter
+            frameFout(:,j)=filter(optimized_a_coef,1,frameFin(:,j));
+            flag_stable=isstable([1],optimized_a_coef);
+            if flag_stable==0
+                steady=0;
+            end
+            
+            %Return the positive values of a
+            TNScoeffs(:,j)= -optimized_a_coef(2:5);    
+            
         end
     else
+        frameFout=zeros(1024,1);
         normalization_coef=zeros(1024,1);
         for i=1:69
             %Store band energy of the band(as before) 
             band_energy = sum((frameFin(long_fft(i,2)+1:long_fft(i,3)+1)).^2);
-
+            
             %Callculate the normalization coefficiencies of the selected values
             %by taking the root square of band's energy
             normalization_coef( long_fft(i,2)+1:long_fft(i,3)+1 )=sqrt(band_energy);
@@ -53,30 +81,25 @@ function [ frameFout] = TNS(frameFin, frameType)
             normalized_mdct(i)=frameFin(i)/normalization_coef(i);
         end
 
-        %Calculate the 4 optimized coeffieciencies a of the FIR prediction filter
+        %Calculate the 4 optimized coeffieciencies a of the FIR prediction filter(a negative is calculated)
         optimized_a_coef = lpc(normalized_mdct,4);
-        optimized_a_coef=transpose(optimized_a_coef(2:5));
+        optimized_a_coef=transpose(optimized_a_coef);
 
         %Quantize the FIR coefficiencies
-        for i=1:4
+        for i=2:5
             optimized_a_coef(i)=quanti( optimized_a_coef(i) );
         end
         
-        %Apply FIR filter
-        frameFout(1)=frameFin(1);
-        frameFout(2)=frameFin(2)-optimized_a_coef(1)*frameFin(1);
-        frameFout(3)=frameFin(3)-optimized_a_coef(1)*frameFin(2)-optimized_a_coef(2)*frameFin(1);
-        frameFout(4)=frameFin(4)-optimized_a_coef(1)*frameFin(3)-optimized_a_coef(2)*frameFin(2)...
-                     -optimized_a_coef(3)*frameFin(3);
-        for i=5:1024
-            frameFout(i)= frameFin(i)-optimized_a_coef(1)*frameFin(i-1)+optimized_a_coef(2)*frameFin(i-2)+...
-            optimized_a_coef(3)*frameFin(i-3)+optimized_a_coef(4)*frameFin(i-4);
+        %Apply FIR filter               
+        frameFout=filter(optimized_a_coef,1,frameFin);
+        flag_stable=isstable(1,optimized_a_coef);
+        if flag_stable==0
+            steady=0;
         end
-
-
         
-    end
-   
+        %Return the positive values of a
+         TNScoeffs= -optimized_a_coef(2:5);
+    end         
 
 end
 
