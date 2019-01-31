00a11fc10d9893c1f42704d4e1854d9de8e0bb2b
teo99516 <40856983+teo99516@users.noreply.github.com>
2019-01-31 03:33:49 +0200

    Add files via upload

--- level2/TNS.m
+++ level2/TNS.m
@@ -1,4 +1,4 @@
-function [ normalization_coef] = TNS(frameFin, frameType)
+function [ frameFout] = TNS(frameFin, frameType)
 
 %frameFin,frameFout: MDCT coefficiences: 128X8 for ESH, else 1024X1 
 
@@ -46,29 +46,30 @@ function [ normalization_coef] = TNS(frameFin, frameType)
         for i=1023:-1:1
             normalization_coef(i)=( normalization_coef(i) + normalization_coef(i+1) )/2;
         end
-        normalized_mdct=zeros(1024,1)
-        normalized_mdct(1)=frameFin(i)/normalization_coef(1);
+        normalized_mdct=zeros(1024,1);
+        normalized_mdct(1)=frameFin(1)/normalization_coef(1);
         for i=2:1024
-            normalization_coef(i)=( normalization_coef(i) + normalization_coef(i-1) )/2;\
+            normalization_coef(i)=( normalization_coef(i) + normalization_coef(i-1) )/2;
             normalized_mdct(i)=frameFin(i)/normalization_coef(i);
         end
 
         %Calculate the 4 optimized coeffieciencies a of the FIR prediction filter
-        optimized_a_coef = lpc(normalized_mdct(),4);
+        optimized_a_coef = lpc(normalized_mdct,4);
+        optimized_a_coef=transpose(optimized_a_coef(2:5));
 
         %Quantize the FIR coefficiencies
         for i=1:4
-            optimized_a_coef=quant( optimized_a_coef(i) );
+            optimized_a_coef(i)=quanti( optimized_a_coef(i) );
         end
         
         %Apply FIR filter
         frameFout(1)=frameFin(1);
         frameFout(2)=frameFin(2)-optimized_a_coef(1)*frameFin(1);
         frameFout(3)=frameFin(3)-optimized_a_coef(1)*frameFin(2)-optimized_a_coef(2)*frameFin(1);
-        frameFout(4)=frameFin(4)-optimized_a_coef(1)*frameFin(3)-optimized_a_coef(2)*frameFin(2)
+        frameFout(4)=frameFin(4)-optimized_a_coef(1)*frameFin(3)-optimized_a_coef(2)*frameFin(2)...
                      -optimized_a_coef(3)*frameFin(3);
         for i=5:1024
-            frameFout(i)= frameFin(i)-optimized_a_coef(1)*frameFin(i-1)+optimized_a_coef(2)*frameFin(i-2)+
+            frameFout(i)= frameFin(i)-optimized_a_coef(1)*frameFin(i-1)+optimized_a_coef(2)*frameFin(i-2)+...
             optimized_a_coef(3)*frameFin(i-3)+optimized_a_coef(4)*frameFin(i-4);
         end
 
@@ -80,7 +81,7 @@ function [ normalization_coef] = TNS(frameFin, frameType)
 end
 
 % A simplified quantizer that quantizes in the larget value of every partition
-function [quantized_value]= quant(in_value)
+function [quantized_value]= quanti(in_value)
 
     for partition=-1.6:0.1:1.6
         if (in_value<=partition)
